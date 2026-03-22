249fa33c3b2b087c410ea625e1477e4a46e1d004
salmazen <salmazen@gmail.com>
2026-03-22 17:51:19 -0400

    fix: update text splitter import

--- src/rag.py
+++ src/rag.py
@@ -12,7 +12,7 @@ from langchain_community.document_loaders import (
     UnstructuredMarkdownLoader,
     WebBaseLoader
 )
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
